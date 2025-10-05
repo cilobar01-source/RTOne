2eb65776cdc0602308010f1b6527dd9cf2cc9ea2
cilobar01-source <cilobar01@gmail.com>
2025-10-06 02:57:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// ============ Firebase Imports ============ //
+// ============ Firebase Config & Init ============ //
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
 
-// ============ Konfigurasi Firebase kamu ============ //
 const firebaseConfig = {
   apiKey: "AIzaSyBUv1Rj7p1vBl0gq75XC7NZeJF0--3fKRk",
   authDomain: "portal-rt-cilosari.firebaseapp.com",
@@ -15,8 +14,7 @@ const firebaseConfig = {
   measurementId: "G-0LRWM5CE1Z"
 };
 
-// ============ Inisialisasi Firebase ============ //
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
